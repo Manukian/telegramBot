61b73eacdebcbe192906cf6a11309ee1e981d242
Ваган Манукян <vaggi@Vagans-MacBook-Pro.local>
2015-07-28 13:28:47 +0300

    new

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
  * Created by ballrider Vaggi on 28/07/15.
  */
 console.log('node worked correctly');
+
+var botApi = require('node-telegram-bot-api');
+console.log(botApi);
+var options = {
+  polling: true
+};
+
+var token = '123731375:AAHeCtHyhTE7W79hw1beOVDt1MSy2PnIqaI';
+
+var bot = new botApi(token, options);
+bot.getMe().then(function (me) {
+  console.log('Hi my name is %s!', me.username);
+});
